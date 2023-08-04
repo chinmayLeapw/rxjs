61df78d745108600951f23245eb89c4936ad8c25
Chinmay Jain <chinmayjain@Chinmays-MacBook-Pro.local>
2023-08-04 09:24:26 +0530

    Add: build folder to repo

---
+++ dist/spec/helpers/subscription.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getRegisteredFinalizers = void 0;
+function getRegisteredFinalizers(subscription) {
+    var _a;
+    if ('_finalizers' in subscription) {
+        return (_a = subscription._finalizers) !== null && _a !== void 0 ? _a : [];
+    }
+    else {
+        throw new TypeError('Invalid Subscription');
+    }
+}
+exports.getRegisteredFinalizers = getRegisteredFinalizers;
+//# sourceMappingURL=subscription.js.map
